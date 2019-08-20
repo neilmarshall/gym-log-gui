32bf1502aed1a329d5f0aea4644c8a135fc7c48d
neilmarshall <neil.marshall@dunelm.org.uk>
2019-08-20 23:18:39 +0100

    add skeleton of login functionality using threads

--- gym_log/__main__.py
+++ gym_log/__main__.py
@@ -1,9 +1,13 @@
+import time
 import tkinter as tk
+from concurrent.futures import ThreadPoolExecutor
 from tkinter import ttk
 
 class LoginWindow(tk.Tk):
 
     def __init__(self):
+
+        self.thread_pool = ThreadPoolExecutor()
 
         super().__init__()
 
@@ -16,10 +20,28 @@ class LoginWindow(tk.Tk):
         ttk.Entry(input_frame).grid(row=0, column=1)
         ttk.Entry(input_frame).grid(row=1, column=1)
 
-        ttk.Button(input_frame, text="Login", command=self.login).grid(row=2, columnspan=2)
+        ttk.Button(input_frame, text="Login",
+                command=self.login).grid(row=2, columnspan=2)
 
     def login(self):
-        print('logging in...')
+        def begin_login():
+            # TODO : This function needs to extract username and password from the GUI and use these to obtain a token
+            print("inner function called")  # TODO : Remove
+            time.sleep(3)  # TODO : Remove
+            print("inner function ended")  # TODO : Remove
+        def end_login(future):
+            print('logged in...')  # TODO : Remove
+            progress_bar.stop()
+            progress_window.destroy()
+        print('logging in...')  # TODO : Remove
+        progress_window = tk.Toplevel(self)
+        progress_frame = ttk.Frame(progress_window)
+        progress_frame.pack()
+        ttk.Label(progress_frame, text="Logging in...").pack()
+        progress_bar = ttk.Progressbar(progress_frame, mode="indeterminate")
+        progress_bar.pack()
+        progress_bar.start()
+        self.thread_pool.submit(begin_login).add_done_callback(end_login)
 
 
 if __name__ == '__main__':
