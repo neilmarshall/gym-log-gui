05a6507ffa6d00d0b866759ea2e61bc76122f5cd
neilmarshall <neil.marshall@dunelm.org.uk>
2019-08-22 21:40:57 +0100

    refactor
    remove instantiation of instance variables outside of __init__ method

--- gym_log/__main__.py
+++ gym_log/__main__.py
@@ -12,8 +12,10 @@ basedir = os.path.split(basedir)[0]
 load_dotenv(os.path.join(basedir, '.env'))
 
 class LoginWindow(tk.Tk):
+    """Subclass of tkinter.Tk  - controls GUI application"""
 
     def __init__(self, logger):
+        """Constructor method"""
 
         super().__init__()
 
@@ -23,40 +25,48 @@ class LoginWindow(tk.Tk):
 
         self.gym_log_controller = GymLogController(self.logger)
 
+        # create menu bar
+        self.create_menu_bar()
+
+        # create login window
+        self.login_window = ttk.Frame(self)
+        self.username_entry = tk.StringVar()
+        self.password_entry = tk.StringVar()
+
+        # launch login window
+        self.launch_login_window()
+
+    def create_menu_bar(self):
+        """Create menu bar for GUI application"""
         menu_bar = tk.Menu(self)
         file_menu = tk.Menu(menu_bar, tearoff=0)
         file_menu.add_command(label="Exit", command=self.quit)
         menu_bar.add_cascade(label='File', menu=file_menu)
         self.config(menu=menu_bar)
 
-        self.load_input_frame()
+    def launch_login_window(self):
+        """Build and launch login window"""
+        self.login_window.pack()
 
-    def load_input_frame(self):
-        self.input_frame = ttk.Frame(self)
-        self.input_frame.pack()
+        ttk.Label(self.login_window, text="Username").grid(row=0, column=0)
+        ttk.Entry(self.login_window, textvariable=self.username_entry).grid(row=0, column=1)
+        self.password_entry.set(os.environ.get('DEFAULT_PASSWORD') or '')
 
-        ttk.Label(self.input_frame, text="Username").grid(row=0, column=0)
-        ttk.Label(self.input_frame, text="Password").grid(row=1, column=0)
+        ttk.Label(self.login_window, text="Password").grid(row=1, column=0)
+        ttk.Entry(self.login_window, textvariable=self.password_entry, show='*').grid(row=1, column=1)
+        self.username_entry.set(os.environ.get('DEFAULT_USERNAME') or '')
 
-        self.username_entry = ttk.Entry(self.input_frame)
-        self.username_entry.grid(row=0, column=1)
-        self.password_entry = ttk.Entry(self.input_frame, show='*')
-        self.password_entry.grid(row=1, column=1)
-
-        ttk.Button(self.input_frame, text="Login",
-                command=self.login).grid(row=2, columnspan=2)
-
-        self.username_entry.insert(0, os.environ.get('DEFAULT_USERNAME') or '')
-        self.password_entry.insert(0, os.environ.get('DEFAULT_PASSWORD') or '')
+        ttk.Button(self.login_window, text="Login", command=self.login).grid(row=2, columnspan=2)
 
     def quit(self):
+        """Exit the application"""
         self.thread_pool.shutdown(False)
         super().quit()
 
     def login(self):
+        """Set a user token on the gym log controller"""
         def begin_login():
-            username = self.username_entry.get()
-            password = self.password_entry.get()
+            username, password = self.username_entry.get(), self.password_entry.get()
             return self.gym_log_controller.set_token(username, password)
         def end_login(future):
             progress_bar.stop()
@@ -65,8 +75,8 @@ class LoginWindow(tk.Tk):
                 messagebox.showwarning("404 - Unauthorized Access",
                         "Login attempt failed - please try again")
             else:
-                self.input_frame.destroy()
-        progress_window = tk.Toplevel(self.input_frame)
+                self.login_window.destroy()
+        progress_window = tk.Toplevel(self.login_window)
         progress_frame = ttk.Frame(progress_window)
         progress_frame.pack()
         ttk.Label(progress_frame, text="Requesting login token...").pack()
@@ -99,11 +109,10 @@ class GymLogController():
                 return False
             else:
                 raise ValueError("unexpected status code received")
-        except Exception:
+        except requests.exceptions.RequestException:
             self.logger.exception("An unhandled exception has been caught attempting to obtain an access token")
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger()
-    LoginWindow(logger).mainloop()
+    LoginWindow(logging.getLogger()).mainloop()
