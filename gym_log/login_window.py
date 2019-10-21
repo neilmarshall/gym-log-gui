09a5fc806bf42fe35280f094d5c89d7ac824108f
neilmarshall <neil.marshall@dunelm.org.uk>
2019-10-22 00:14:49 +0100

    add 'remember me' functionality

--- gym_log/login_window.py
+++ gym_log/login_window.py
@@ -1,4 +1,5 @@
 import os
+import re
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
@@ -22,22 +23,26 @@ class LoginWindow(ttk.Frame):
 
         self._username_entry = tk.StringVar()
         self._password_entry = tk.StringVar()
+        self._remember_login = tk.IntVar()
 
     def launch(self, successful_login_callback):
         """Build and launch login window"""
         self.pack()
 
         ttk.Label(self, text="Username").grid(row=0, column=0)
-        ttk.Entry(self, textvariable=self._username_entry).grid(row=0, column=1)
+        ttk.Entry(self, textvariable=self._username_entry).grid(row=0, column=1, columnspan=2)
         self._password_entry.set(os.environ.get('DEFAULT_PASSWORD') or '')
 
         ttk.Label(self, text="Password").grid(row=1, column=0)
-        ttk.Entry(self, textvariable=self._password_entry, show='*').grid(row=1, column=1)
+        ttk.Entry(self, textvariable=self._password_entry, show='*').grid(row=1, column=1, columnspan=2)
         self._username_entry.set(os.environ.get('DEFAULT_USERNAME') or '')
 
         ttk.Button(self, text="Login",
                 command=lambda: self._login(successful_login_callback)) \
-           .grid(row=2, columnspan=2)
+           .grid(row=2, column=1)
+
+        ttk.Checkbutton(self, text="Remember me", variable=self._remember_login).grid(row=2, column=2)
+        self._remember_login.set(True)
 
     def _login(self, successful_login_callback):
         """Set a user token on the gym log controller"""
@@ -45,6 +50,8 @@ class LoginWindow(ttk.Frame):
             username, password = self._username_entry.get(), self._password_entry.get()
             is_login_successful = self._gym_log_controller.check_token(username, password)
             if is_login_successful:
+                if self._remember_login.get():
+                    self._store_login_details(username, password)
                 self._gym_log_controller.set_exercises()
             return is_login_successful
         def end_login(future):
@@ -64,3 +71,15 @@ class LoginWindow(ttk.Frame):
         progress_bar.pack()
         progress_bar.start()
         self._thread_pool.submit(begin_login).add_done_callback(end_login)
+
+    def _store_login_details(self, username, password):
+        def keep_setting(setting):
+            username_match = re.match(r'^\s*DEFAULT_USERNAME=.*$', setting)
+            password_match = re.match(r'\s*DEFAULT_PASSWORD=.*$', setting)
+            return setting and not username_match and not password_match
+        with open('.env') as f:
+            current_settings = f.read().split('\n')
+        with open('.env', 'w') as f:
+            f.write(f'DEFAULT_USERNAME={username}\n')
+            f.write(f'DEFAULT_PASSWORD={password}\n')
+            f.writelines('\n'.join(filter(keep_setting, current_settings)))
